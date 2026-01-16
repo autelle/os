92ec3c159af35456b5e9e3b8a1a90912df44cd0a
autelle <aut.tea7@gmail.com>
2026-01-15 19:54:33 -0600

    gitignore

--- obj/flanterm/src/flanterm.c.d
+++
@@ -1,4 +0,0 @@
-obj/flanterm/src/flanterm.c.o: flanterm/src/flanterm.c \
- flanterm/src/flanterm.h flanterm/src/flanterm_private.h
-flanterm/src/flanterm.h:
-flanterm/src/flanterm_private.h:
--- obj/src/main.c.d
+++
@@ -1,7 +0,0 @@
-obj/src/main.c.o: src/main.c src/limine.h flanterm/src/flanterm.h \
- flanterm/src/flanterm_backends/fb.h \
- flanterm/src/flanterm_backends/../flanterm.h
-src/limine.h:
-flanterm/src/flanterm.h:
-flanterm/src/flanterm_backends/fb.h:
-flanterm/src/flanterm_backends/../flanterm.h:

d26c2dcc3aea42ffd22f7d38614d952fef96af1d
autelle <aut.tea7@gmail.com>
2026-01-15 19:42:53 -0600

    Initial commit

---
+++ obj/flanterm/src/flanterm.c.d
@@ -0,0 +1,4 @@
+obj/flanterm/src/flanterm.c.o: flanterm/src/flanterm.c \
+ flanterm/src/flanterm.h flanterm/src/flanterm_private.h
+flanterm/src/flanterm.h:
+flanterm/src/flanterm_private.h:
---
+++ obj/src/main.c.d
@@ -0,0 +1,7 @@
+obj/src/main.c.o: src/main.c src/limine.h flanterm/src/flanterm.h \
+ flanterm/src/flanterm_backends/fb.h \
+ flanterm/src/flanterm_backends/../flanterm.h
+src/limine.h:
+flanterm/src/flanterm.h:
+flanterm/src/flanterm_backends/fb.h:
+flanterm/src/flanterm_backends/../flanterm.h:
